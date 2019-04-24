c115b2a1344d22bcc1d29e8fc58b41f273dfa34b
shivchangela1 <shivc@uw.edu>
2019-04-24 15:05:43 -0700

    Finished code

--- TipCalc/app/src/main/java/edu/washington/shivc/tipcalc/MainActivity.kt
+++ TipCalc/app/src/main/java/edu/washington/shivc/tipcalc/MainActivity.kt
@@ -7,13 +7,12 @@ import android.widget.Button
 import android.widget.EditText
 import android.text.TextWatcher
 import android.widget.*
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var amount: EditText
+    lateinit var total: EditText
     lateinit var btn: Button
-    lateinit var tipPercentage: Spinner
+    lateinit var tip: Spinner
 
 
 
@@ -21,13 +20,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        tipPercentage = findViewById(R.id.spinner)
+        tip = findViewById(R.id.spinner)
         btn = findViewById(R.id.tipButton)
         btn.isEnabled = false
-        amount = findViewById(R.id.amountInput)
+        total = findViewById(R.id.amountInput)
         btn.setOnClickListener {
-            val fullAmount = amount.text.substring(1, amount.length()).toDouble()
-            val percent = tipPercentage.selectedItem.toString().dropLast(1).toInt()
+            val fullAmount = total.text.substring(1, total.length()).toDouble()
+            val percent = tip.selectedItem.toString().dropLast(1).toInt()
             val tip = "%.2f".format(fullAmount * (percent * 0.01))
             Toast.makeText(applicationContext, "$$tip", Toast.LENGTH_SHORT).show()
         }
@@ -35,35 +34,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
-        amount.addTextChangedListener(object : TextWatcher {
-            val textBox: TextView = amount
+        total.addTextChangedListener(object : TextWatcher {
+            val textBox: TextView = total
             var oldText = ""
             override fun afterTextChanged(p0: Editable?) {
-                if (amount.length() >= 3) {
-                    if (amount.text.contains('.')) {
-                        if (amount.text.indexOf('.', 0, true) == amount.length() - 4) {
+                if (total.length() >= 3) {
+                    if (total.text.contains('.')) {
+                        if (total.text.indexOf('.', 0, true) == total.length() - 4) {
                             textBox.text = oldText
-                            amount.setSelection(amount.length())
+                            total.setSelection(total.length())
 
                         }
                     }
                 }
             }
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                oldText = amount.text.toString()
+                oldText = total.text.toString()
             }
 
             override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                btn.isEnabled = amount.text.isNotBlank()
-                if (amount.length() >= 1) {
-                    if (amount.text.toString()[0] != '$') {
-                        textBox.text = "$${amount.text}"
-                        amount.setSelection(amount.length())
+                btn.isEnabled = total.text.isNotBlank()
+                if (total.length() >= 1) {
+                    if (total.text.toString()[0] != '$') {
+                        textBox.text = "$${total.text}"
+                        total.setSelection(total.length())
                     }
                 }
 
-                if (amount.length() >= 3) {
-                    if (amount.text.toString()[2] == '$') {
+                if (total.length() >= 3) {
+                    if (total.text.toString()[2] == '$') {
                         val newText = textBox.text.toString().replace("$", "", true)
                         textBox.text = "$${newText}"
                     }
